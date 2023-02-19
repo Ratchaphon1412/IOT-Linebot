ee2cfc4584b0813f769430fb1ed4f4ae39b653f7
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-19 18:13:25 +0700

    connect line notification to django rest and create api that send the message

--- services/api/views.py
+++ services/api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from decouple import config
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from parinya import LINE
+
+
+
+
+class LineBotView(APIView):
+    def post(self, request):
+         
+        text = request.data.get('text')
+        print(request.data)
+        if request.headers.get('Authorization') is not None :
+            token = request.headers.get('Authorization').split('Bearer ')[1]
+            print(token)
+            lineNotification = LINE(token)
+            lineNotification.sendtext(text)
+            del(lineNotification)
+        else:
+            lineNotification = LINE(config('TOKEN_NOTIFICATION_LINE'))
+            lineNotification.sendtext(text)
+            del(lineNotification)
+        
+        return Response({'status': 200})
+        
+    def get(self, request):
+       
+        return Response({'status': 200})
+    
+    
